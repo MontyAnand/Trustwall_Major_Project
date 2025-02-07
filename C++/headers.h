6e135b6f440b75a47d9ab127f36abcc6814f153c
Vivek Kumar <vk2818970@gmail.com>
2025-02-07 12:25:48 +0530

    VPN Code added

--- C++/headers.h
+++ C++/headers.h
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <filesystem>
+#include <array>
+#include <cstring>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <stdexcept>
+#include <iterator>
+#include <cassert>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,9 +22,19 @@
 #include <netinet/in.h>
 #include <sys/epoll.h>
 #include <fcntl.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <qrencode.h>
+#include <png.h>
 
 
-#define PORT 8080
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <atomic>
+
+
+#define SERVER_PORT 8080
 #define BUFFER_SIZE 1024
 #define TABLE_NAME "Remote_Access"
 #define SRC_PORT_SET "src"
@@ -36,6 +58,58 @@ class Firewall {
 
     public:
 };
+
+class QR {
+    public:
+        static void write_png(const char *, unsigned char *, int , int);
+        static std::string file_to_base64(const std::string &);
+        static std::string generate_qr_base64(const std::string &text, int scale = 10);
+};
+
+class VPN {
+    private:
+        int TIMEOUT_PERIOD;
+        std::string publicInterface;
+        std::string endPoint;
+        std::string publicIP;
+        std::string server_public_key;
+        std::uint64_t ip_pool;
+        std::uint16_t PORT;
+        std::string netmask;
+
+        std::mutex mtx;
+        std::thread cleaner;
+        std::atomic <bool> running;
+
+        std::map<std::string,std::uint16_t>record;
+
+        std::string getPublicInterface();
+        std::string getEndPoint();
+        std::string compressData (std::string &);
+        std::uint16_t getAvailableID();
+        std::uint16_t generateClientConfiguration(std::string&, std::string&);
+        std::string prepareIP(std::uint16_t);
+        bool generateServerKeys();
+        bool setupServer();
+        bool generateConfigurationFile(std::string&);
+        bool vpnInterfaceSetup();
+        bool isNumber(std::string &);
+        bool checkConnectivity();
+        void revokeIP(std::uint16_t);
+        void monitorClient();
+        void parseString(std::string &, std::map<std::string,int> &);
+        void generateQRCode(std::uint16_t, std::string&, std::string&);
+        void addFirewallRules();
+        
+
+    public:
+        VPN();
+        void printData();
+        std::string getIP();
+        std::uint16_t acceptConnectionRequest();
+        ~VPN ();
+};
+
 
 
 class Antivirus{
--- C++/server.cpp
+++ C++/server.cpp
@@ -1,13 +1,16 @@
 #include "headers.h"
 
-void Server::setNonBlocking(int fd){
+void Server::setNonBlocking(int fd)
+{
     int flags = fcntl(fd, F_GETFL, 0);
     fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 }
 
-int Server::createServerSocket(){
+int Server::createServerSocket()
+{
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
+    if (server_fd == -1)
+    {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
@@ -16,18 +19,20 @@ int Server::createServerSocket(){
     int opt = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    struct sockaddr_in server_addr {};
+    struct sockaddr_in server_addr{};
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(SERVER_PORT);
 
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
         perror("bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, SOMAXCONN) == -1) {
+    if (listen(server_fd, SOMAXCONN) == -1)
+    {
         perror("listen failed");
         close(server_fd);
         exit(EXIT_FAILURE);
@@ -37,37 +42,45 @@ int Server::createServerSocket(){
     return server_fd;
 }
 
-void Server::addToInputEventLoop(int fd){
-    struct epoll_event event {};
+void Server::addToInputEventLoop(int fd)
+{
+    struct epoll_event event{};
     event.events = EPOLLIN;
     event.data.fd = fd;
     epoll_ctl(epollFd, EPOLL_CTL_ADD, fd, &event);
 }
 
-void Server::addToOutputEventLoop (int fd){
-    struct epoll_event event {};
+void Server::addToOutputEventLoop(int fd)
+{
+    struct epoll_event event{};
     event.events = EPOLLOUT;
     event.data.fd = fd;
     epoll_ctl(epollFd, EPOLL_CTL_ADD, fd, &event);
 }
 
-void Server::eventLoop(){
-    while (true) {
+void Server::eventLoop()
+{
+    while (true)
+    {
         int event_count = epoll_wait(epollFd, events, MAX_EVENTS, -1);
-        if (event_count == -1) {
+        if (event_count == -1)
+        {
             perror("epoll_wait failed");
             break;
         }
 
-        for (int i = 0; i < event_count; i++) {
+        for (int i = 0; i < event_count; i++)
+        {
             int fd = events[i].data.fd;
 
-            if (fd == serverSocketFd) {
+            if (fd == serverSocketFd)
+            {
                 // Accept new client
-                struct sockaddr_in client_addr {};
+                struct sockaddr_in client_addr{};
                 socklen_t client_len = sizeof(client_addr);
-                int client_fd = accept(serverSocketFd, (struct sockaddr*)&client_addr, &client_len);
-                if (client_fd == -1) {
+                int client_fd = accept(serverSocketFd, (struct sockaddr *)&client_addr, &client_len);
+                if (client_fd == -1)
+                {
                     perror("accept failed");
                     continue;
                 }
@@ -76,34 +89,40 @@ void Server::eventLoop(){
                 addToInputEventLoop(client_fd);
 
                 std::cout << "New client connected: " << client_fd << std::endl;
-
-            } else if (events[i].events & EPOLLIN) {
+            }
+            else if (events[i].events & EPOLLIN)
+            {
                 // Read data from client
                 char buffer[BUFFER_SIZE] = {0};
                 int bytes_read = read(fd, buffer, sizeof(buffer));
 
-                if (bytes_read <= 0) {
+                if (bytes_read <= 0)
+                {
                     std::cout << "Client disconnected: " << fd << std::endl;
                     epoll_ctl(epollFd, EPOLL_CTL_DEL, fd, nullptr);
                     close(fd);
-                } else {
+                }
+                else
+                {
                     std::cout << "Received: " << buffer << " from client " << fd << std::endl;
                 }
+            }
+            else if (events[i].events & EPOLLOUT)
+            {
 
-            } else if(events[i].events & EPOLLOUT){
-                
                 // Send data to client
 
                 addToInputEventLoop(fd);
-
             }
         }
     }
 }
 
-Server::Server(){
+Server::Server()
+{
     epollFd = epoll_create1(0);
-    if (epollFd == -1) {
+    if (epollFd == -1)
+    {
         perror("epoll_create1 failed");
         exit(EXIT_FAILURE);
     }
