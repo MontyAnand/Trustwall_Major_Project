d3fb4ea3422bec986af48ec8e1add2e3c0ea6305
Sudeshna <sudeshnabasak02@gmail.com>
2025-02-10 14:27:28 +0530

    VPN UI change

---
+++ Web_Interface/src/components/Incoming_net_traffic.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IncomingTraffic=()=>{
+    return (
+        <div className='IncTraffic_container'>
+            <h2>Incoming traffic Information</h2>
+        </div>
+    );
+}
+
+export default IncomingTraffic;
--- Web_Interface/src/pages/Dashboard.js
+++ Web_Interface/src/pages/Dashboard.js
@@ -1,4 +1,8 @@
 import Sidebar from "../components/Sidebar";
+import MemoryInfo from "../components/Memory_info";
+import DiskInfo from "../components/Disk_info";
+import IncomingTraffic from "../components/Incoming_net_traffic";
+import OutgoingTraffic from "../components/Outgoing_net_traffic";
 import './dashboard.css'
 
 export default function Dashboard(){
@@ -7,6 +11,12 @@ export default function Dashboard(){
             <Sidebar />
             <div className="dashboard_container">
                 <h2>Dashboard</h2>
+                <div className="dashboard_inner_container">
+                    <MemoryInfo/>
+                    <DiskInfo/>
+                    <IncomingTraffic/>
+                    <OutgoingTraffic/>
+                </div>
             </div>
         </>
     )
