48362f7c976a0739590da76fbefd74eb3d41bfed
Sudeshna <sudeshnabasak02@gmail.com>
2025-03-01 00:47:10 +0530

    Updated Login page

--- Web_Interface/src/pages/Login.js
+++ Web_Interface/src/pages/Login.js
@@ -3,6 +3,10 @@ import { useAuth } from "../Contexts/authContex";
 import { useNavigate } from "react-router-dom";
 import { useSocket } from "../Contexts/socketContex";
 import './Login.css';
+import hide from '../Images/hide.png';
+import view from '../Images/view.png';
+import key from '../Images/key.png';
+import face_id from '../Images/face-id.png';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -11,6 +15,8 @@ function Login() {
   const { setIsAuthenticated } = useAuth();
   const { socket } = useSocket();
   const navigate = useNavigate();
+  const [isPassword,setIsPassword] = useState('password');
+  const [PasswordContent,setPasswordContent] = useState(hide);
 
   useEffect(()=>{
     if (!socket) return;
@@ -40,26 +46,44 @@ function Login() {
   };
 
   return (
-    <div className="login-page">
-      <h1>Login</h1>
-      <div className="form-group">
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      {error && <p className="error">{error}</p>}
-      <button onClick={handleLogin}>Login</button>
+    <div className='login_container'>
+          <div className="login-page">
+          <h1>Login</h1>
+          <div className="form-group-1">
+            <div className='form-internal-container' >
+            <img src={face_id} alt='user-img'/>
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            </div>
+          </div>
+          <div className="form-group-2">
+          <div className='form-internal-container' >
+          <img src={key} alt='key-img'/>
+            <input
+              type={isPassword}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+            <img className='password-content' src={PasswordContent} alt='Img' onClick={()=>{
+              if(isPassword === 'password'){
+                  setIsPassword('text');
+                  setPasswordContent(view);
+              }
+              else if(isPassword === 'text'){
+                  setIsPassword('password');
+                  setPasswordContent(hide);
+              }
+            }}/>
+          </div>
+          {error && <p className="error">{error}</p>}
+          <button onClick={handleLogin}>Login</button>
+        </div>
     </div>
   );
 }
